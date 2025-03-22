8e7caf7d1fe7d8398d0cf010278dbdbf04a4ce42
Khaliq <bias.khaliq@gmail.com>
2025-03-22 08:12:07 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -21,11 +21,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aospb_coral.mk \
-    $(LOCAL_DIR)/aospb_flame.mk \
-    $(LOCAL_DIR)/aospb_coral_hwasan.mk \
-    $(LOCAL_DIR)/aospb_flame_hwasan.mk
+    $(LOCAL_DIR)/lineage_coral.mk \
+    $(LOCAL_DIR)/lineage_flame.mk \
+    $(LOCAL_DIR)/lineage_coral_hwasan.mk \
+    $(LOCAL_DIR)/lineage_flame_hwasan.mk
 
 COMMON_LUNCH_CHOICES := \
-    aospb_coral-trunk_staging-userdebug \
-    aospb_flame-trunk_staging-userdebug
+    lineage_coral-trunk_staging-userdebug \
+    lineage_flame-trunk_staging-userdebug
