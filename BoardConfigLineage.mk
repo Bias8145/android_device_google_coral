a71d199440f9626100dbce3aae81b7fda65f668b
Khaliq <bias.khaliq@gmail.com>
2025-03-22 08:13:14 +0700

    Update BoardConfigLineage.mk

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -25,7 +25,7 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED := 1
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/coral/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # Needed for camera
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
@@ -36,7 +36,7 @@ AB_OTA_PARTITIONS += \
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Reserve space for gapps install
--include vendor/aosp/config/BoardConfigReservedSize.mk
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/google/coral/sepolicy-lineage/dynamic
