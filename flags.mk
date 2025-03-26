e7478176b741d52b11aa9962a6258ecb4528a812
Khaliq <bias.khaliq@gmail.com>
2025-03-26 13:39:27 +0700

    Update flags.mk

--- flags.mk
+++ flags.mk
@@ -1,19 +1,8 @@
 #
-# Copyright (C) 2025 Morpheus
+# Copyright (C) 2025 aosPB-Project
 #
 
-# Flags for RisingOS
-RISING_MAINTAINER=khaliq
-RISING_PACKAGE_TYPE := HomeMade
-
-# disable/enable blur support
-TARGET_ENABLE_BLUR := true
-
-# whether to ship aperture camera
-PRODUCT_NO_CAMERA := true
-
-# GMS build flags
-WITH_GMS := true
-
-# Wether to ship pixel launcher and set it as default launcher
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
+# Flags for aosPB
+CUSTOM_BUILDTYPE := special
+IS_SIGNED := true
+CUSTOM_MAINTAINER := Khaliq
